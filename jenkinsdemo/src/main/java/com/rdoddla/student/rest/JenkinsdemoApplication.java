eff4515ad8c04898358e5f6cf04714b88e2764cc
user <user@192.168.56.1>
2022-12-31 17:02:30 +0530

    3rd commit

--- jenkinsdemo/src/main/java/com/rdoddla/student/rest/JenkinsdemoApplication.java
+++ jenkinsdemo/src/main/java/com/rdoddla/student/rest/JenkinsdemoApplication.java
@@ -8,7 +8,7 @@ public class JenkinsdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsdemoApplication.class, args);
-		System.out.println("jenkins demo..");
+		System.out.println("****jenkins demo..");
 	}
 
 }
