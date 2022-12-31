a9d7b1faf3815fdd4386e3c5ea5f033c02d1d2ea
user <user@192.168.56.1>
2022-12-31 18:11:26 +0530

    comment added

--- jenkinsdemo/src/main/java/com/rdoddla/student/rest/JenkinsdemoApplication.java
+++ jenkinsdemo/src/main/java/com/rdoddla/student/rest/JenkinsdemoApplication.java
@@ -9,6 +9,7 @@ public class JenkinsdemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsdemoApplication.class, args);
 		System.out.println("****jenkins demo..");
+		System.out.println("testing...");
 	}
 
 }
