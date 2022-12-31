e307f9409741a51f64c499e30ce293d1b459afb9
user <user@192.168.56.1>
2022-12-31 15:14:27 +0530

    first commit

---
+++ jenkinsdemo/src/main/java/com/rdoddla/student/rest/JenkinsdemoApplication.java
@@ -0,0 +1,14 @@
+package com.rdoddla.student.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsdemoApplication.class, args);
+		System.out.println("jenkins demo..");
+	}
+
+}
